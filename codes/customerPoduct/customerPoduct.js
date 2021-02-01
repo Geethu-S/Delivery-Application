d2c82af88af614cf1c30d1ca30e6120193b2fa3c
Geethu-S <74547350+Geethu-S@users.noreply.github.com>
2021-02-01 11:16:48 +0530

    Update customerPoduct.js

--- codes/customerPoduct/customerPoduct.js
+++ codes/customerPoduct/customerPoduct.js
@@ -1,63 +1,25 @@
-import { LightningElement } from 'lwc';
-const columns = [
-    {label: 'Product id', fieldName: 'productId', type: 'number'},
-    {label: 'Product name', fieldName: 'productName', type: 'text'},
-    {label: 'Price', fieldName: 'price', type: 'currency', typeAttributes: { currencyCode: 'USD'}},
-    {label: 'Quantity', fieldName: 'quantity', type: 'number'},
-    {label: 'Totalprice', fieldName: 'totalprice', type: 'currency', typeAttributes: { currencyCode: 'USD'}},
-    {label: 'Driver name', fieldName: 'drivername', type: 'text'},
-    {label:'Driver contact number', fieldName:'drivercontactnumber',type:'phone',},
-    {label:'Location', fieldName:'location',type:'text'},
-];
-
-const data = [{
-                   id: 'a',
-                   productId: 111,
-                   productName: 'Mobile',
-                   price: 25000,
-                   quantity: 2,
-                   totalprice: 50000,
-                   drivername: 'George',
-                   drivercontactnumber: '222-444-555',
-                   location: 'Baltimore',
-                   trendIcon: 'utility:down'
-               },
-               {
-                   id: 'b',
-                   productId: 112,
-                   productName: 'Laptop',
-                   price: 35000,
-                   quantity: 2,
-                   totalprice: 70000,
-                   drivername: 'James',
-                   drivercontactnumber: '333-777-888',
-                   location: 'College Park',
-                   trendIcon: 'utility:up'
-               },
-               {
-                   id: 'c',
-                   productId: 113,
-                   productName: 'Hard Disk',
-                   price: 3000,
-                   quantity: 3,
-                   totalprice: 9000,
-                   drivername: 'Joseph',
-                   drivercontactnumber: '222-666-999',
-                   location: 'Maryland',
-               }
-            
-            ];
+import { LightningElement , api } from 'lwc';
+import PRODUCT_ID_FIELD from '@salesforce/schema/Delivery.Product_Id';
+import PRODUCT_NAME_FIELD from '@salesforce/schema/Delivery.Product_Name';
+import QUANTITY_FIELD from '@salesforce/schema/Delivery.Quantity';
+import PRICE_FIELD from '@salesforce/schema/Delivery.Price';
+import TOTAL_PRICE_FIELD from '@salesforce/schema/Delivery.Total_Price';
+import DRIVER_NAME_FIELD from '@salesforce/schema/Delivery.Driver_Name';
+import DRIVER_CONTACT_NO from '@salesforce/schema/Delivery.Driver_Contact_No';
+import LOCATION_FIELD from '@salesforce/schema/Delivery.Location';
 export default class CustomerPoduct extends LightningElement {
-    data = data;
-    columns = columns;
-
-    getSelectedName(event) {
-        const selectedRows = event.detail.selectedRows;
-        // Display that fieldName of the selected rows
-        for (let i = 0; i < selectedRows.length; i++){
-            alert("You selected: " + selectedRows[i].productName);
-        }
+    @api recordId;
+    @api objectApiName;
+    fields = [PRODUCT_ID_FIELD, PRODUCT_NAME_FIELD, QUANTITY_FIELD,PRICE_FIELD,TOTAL_PRICE_FIELD,DRIVER_NAME_FIELD,DRIVER_CONTACT_NO,LOCATION_FIELD];    
+    handleSubmit(event){
+        //you can change values from here
+        //const fields = event.detail.fields;
+        //fields.Name = 'My Custom  Name'; // modify a field
+        console.log('Delivery detail : ',event.detail.fields);
+        console.log('Delivery name : ',event.detail.fields.Name);
     }
+   
+    
 
 }
 
